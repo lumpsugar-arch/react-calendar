78f8618674d5bc1f8f43087314afb3d454efd206
Roman Afanasyev <afanasyev.r@yandex.ru>
2019-08-06 13:32:13 +0300

    Express router init

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express'),
       path = require('path'),
       mongoose = require('mongoose'),
-      keys = require('./keys');
+      keys = require('./keys'),
+      eventRouter = require('./routes/event'),
+      bodyParser = require('body-parser');
 
 const app = express(),
       port = process.env.PORT || 5000,
@@ -11,6 +13,8 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true} )
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
+app.use(bodyParser.json());
+app.use('/api/events', eventRouter);
 app.use(express.static(clientPath));
 
 app.listen(port, () => {
---
+++ routes/event.js
@@ -0,0 +1,36 @@
+const express = require('express'),
+      Event = require('../models/Event');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const events = await Event.find({});
+  res.status(200).json(events)
+});
+
+router.post('/', async (req, res) => {
+  const eventData = {
+    title: req.body.title,
+    dateStart: req.body.dateStart,
+    dateEnd: req.body.dateEnd
+  };
+
+  const event = new Event(eventData);
+
+  await event.save();
+  res.status(201).json(event)
+});
+
+router.put('/:id', async (req, res) => {
+  const event = await Event.findByIdAndUpdate({_id: req.params.id}, req.body);
+  res.status(200).json(event)
+});
+
+router.delete('/:id', async (req, res) => {
+  await Event.remove({_id: req.params.id});
+  res.status(200).json({
+    message: 'Event deleted'
+  })
+});
+
+module.exports = router;
